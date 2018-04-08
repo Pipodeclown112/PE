b8acfd88e6b456bb487e4ef5814fcfb010dd1cf5
Floris <floris_golbach@hotmail.com>
2018-04-08 16:10:10 +0200

    Implement loop unrolling

--- Assignment1/matmul/mm_kernel.c
+++ Assignment1/matmul/mm_kernel.c
@@ -7,8 +7,15 @@ void matrix_mult(int m, int n, int p, float *A, float *B, float *C) {
   for(i=0; i<m; i++) {
     for(j=0; j<p; j++) {
       C[i*p+j]=0;
-      for(k=0; k<n; k++) {
+      for(k=0; k<n; k+=8) {
         C[i*p+j] += A[i*n+k]*B[k*p+j];
+        C[i*p+j] += A[i*n+k+1]*B[(k+1)*p+j];
+        C[i*p+j] += A[i*n+k+2]*B[(k+2)*p+j];
+        C[i*p+j] += A[i*n+k+3]*B[(k+3)*p+j];
+        C[i*p+j] += A[i*n+k+4]*B[(k+4)*p+j];
+        C[i*p+j] += A[i*n+k+5]*B[(k+5)*p+j];
+        C[i*p+j] += A[i*n+k+6]*B[(k+6)*p+j];
+        C[i*p+j] += A[i*n+k+7]*B[(k+7)*p+j];
       }
     }
   }
