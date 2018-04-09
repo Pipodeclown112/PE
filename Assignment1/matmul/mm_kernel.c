41b4ae645e4ac702e8cb54f4ef27c757a352eaa6
Floris <floris_golbach@hotmail.com>
2018-04-09 15:50:36 +0200

    Process loop unrolling remainder

--- Assignment1/matmul/mm_kernel.c
+++ Assignment1/matmul/mm_kernel.c
@@ -4,6 +4,9 @@
 void matrix_mult(int m, int n, int p, float *A, float *B, float *C) {
   int i, j, k;
 
+  // Calculate remainder to check if loop unrolling is cleanly divisible.
+  int rem = k % n;
+
   for(i=0; i<m; i++) {
     for(j=0; j<p; j++) {
       C[i*p+j]=0;
@@ -16,6 +19,15 @@ void matrix_mult(int m, int n, int p, float *A, float *B, float *C) {
         C[i*p+j] += A[i*n+k+5]*B[(k+5)*p+j];
         C[i*p+j] += A[i*n+k+6]*B[(k+6)*p+j];
         C[i*p+j] += A[i*n+k+7]*B[(k+7)*p+j];
+      }
+
+      // Process remaining leftover, if any.
+      if(rem){
+        k-=8;
+        while(k<n){
+          C[i*p+j] += A[i*n+k]*B[k*p+j];
+          k++;
+        }        
       }
     }
   }
